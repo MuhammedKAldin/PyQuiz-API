ff0725d13c5268b2058d85f82f2c83561e7041d9
SubstanceShift <54613628+MuhammedKAldin@users.noreply.github.com>
2022-07-24 02:32:34 +0200

    Creating Exium base design

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import requests
 import json
+import os
+import requests
 
-response = requests.get("https://opentdb.com/api.php?amount=10")
+response = requests.get("https://opentdb.com/api.php?amount=10&difficulty=hard")
 
 def requestJson(obj):
     # create a formatted string of the Python JSON object
@@ -9,5 +10,22 @@ def requestJson(obj):
     return text
 
 # Parse JSON into an object with attributes corresponding to dict keys.
-x = json.loads(requestJson(response.json()["results"]))
-print(x[0]["category"])
+data = json.loads(requestJson(response.json()["results"]))
+#print(x[0]["category"])
+
+#--------------------- Design ---------------------#
+from ast import If
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.uic import loadUi
+import sys
+
+class Main(QMainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
+        loadUi('Login.ui', self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ui = Main()
+    ui.show()
+    app.exec_() 
